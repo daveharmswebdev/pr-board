11b88db702ecaf0a17d2e5fe8c47504576288e87
Dave Harms <daveharmswebdev@gmail.com>
2016-11-28 14:06:55 -0600

    established testing with mocha and chai -- true equals true

---
+++ test/server.spec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const request = require('supertest')
+const {expect} = require('chai')
+
+describe('test suite', () => {
+	it('true should equal true', () => {
+		expect(true).to.equal(true)
+	})
+})
