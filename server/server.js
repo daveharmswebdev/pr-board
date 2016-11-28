08c6ba6c50506b05af79c9dbc2f1cac23f964a3e
Dave Harms <daveharmswebdev@gmail.com>
2016-11-28 14:43:31 -0600

    now I have a cookiee, too. ho! ho! ho!

--- server/server.js
+++ server/server.js
@@ -2,9 +2,29 @@
 
 const express = require('express')
 const app = express()
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
+
 const routes = require('../routes/')
 
 const PORT = process.env.PORT || 3000
+
+// middlewares
+app.use(session({
+  store: new RedisStore({
+    url: process.env.REDIS_URL || 'redis://localhost:6379',
+  }),
+  resave: false,
+  saveUninitialized: true,
+  secret: 'davesecretkey',
+}))
+
+app.use((req, res, next) => {
+	console.log('req.session', req.session)
+	app.locals.user = req.session && req.session.user_name
+	next()
+})
+
 
 app.use(routes)
 
