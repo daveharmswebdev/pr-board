f343beebbfb5c2b32745d302ca51a85f11c57bb6
Dave Harms <daveharmswebdev@gmail.com>
2016-11-30 09:10:32 -0600

    created formatProfile method in Profile, tdd profile.create

--- model/profile.js
+++ model/profile.js
@@ -5,6 +5,28 @@ let bookshelf = require('../bookshelf')
 let Profile = bookshelf.Model.extend({
 	tableName: 'profile',
 	idAttribute: 'profile_id'
+}, {
+	formatProfile: model => {
+		const heightString = model => {
+			const height = parseInt(model.attributes.height)
+			const feet = Math.floor(height/12)
+			const inches = height%feet
+			return `${feet}' ${inches}"`
+		}
+		const middleInitial = model => 
+			model.attributes.middle_initial.length > 0 ? `${model.attributes.middle_initial}.` : ''
+		const fullName = model => 
+			`${model.attributes.first_name} ${middleInitial(model)} ${model.attributes.last_name}`
+		const profile = {
+			profile_id: model.attributes.profile_id,
+			user_name: model.attributes.user_name,
+			full_name: fullName(model),
+			height_string: heightString(model),
+			weight: parseInt(model.attributes.weight),
+			password: 'awesome'
+		}
+		return profile		
+	}
 })
 
 module.exports = Profile
