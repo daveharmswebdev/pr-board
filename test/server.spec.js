933812fae1e20cf7a1dafe06e147aaeaa4382953
Dave Harms <daveharmswebdev@gmail.com>
2016-12-02 11:47:24 -0600

    added error handling and test for error handling

--- test/server.spec.js
+++ test/server.spec.js
@@ -18,3 +18,31 @@ describe('hello world test', () => {
 			.expect(200, done)
 	})
 })
+
+describe('route not found test', () => {
+	it('should respond with 404 with `get /api/nowhere`', done => {
+		request(app)
+			.get('/api/nowhere')
+			.expect('Content-Type', /json/)
+			.expect(404)
+			.end((err, res) => {
+				expect(res.body).to.have.property('message')
+				expect(res.body.message).to.equal('route not found')
+				done()
+			})
+	})
+})
+
+describe('error handling', () => {
+	it('should give a proper error message', done => {
+		request(app)
+			.get('/api/profile/1')
+			.expect('Content-Type', /json/)
+			.expect(500)
+			.end((err, res) => {
+				expect(res.body).to.have.property('message')
+				expect(res.body.message).to.equal('internal error')
+				done()
+			})
+	})
+})
