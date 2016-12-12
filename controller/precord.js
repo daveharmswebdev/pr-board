13be22be2592a9f8e920c0df43fdba8ab43905e8
Dave Harms <daveharmswebdev@gmail.com>
2016-12-12 15:27:21 -0600

    established personal records route and controller and test suite

---
+++ controller/precord.js
@@ -0,0 +1,5 @@
+'use strict'
+
+module.exports.index = (req, res) => {
+	res.json({"message":"new pr"})
+}
---
+++ routes/precord.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const {Router} = require('express')
+const router = Router()
+const precord = require('../controller/precord')
+
+router.get('/api/precord', precord.index)
+
+// router.get('/api/precord/:profile_id', precord.indexByProfileId)
+
+// router.post('/api/precord', precord.create)
+
+// router.patch('/api/precord', precord.update)
+
+// router.delete('/api/precord', precord.destroy)
+
+module.exports = router
