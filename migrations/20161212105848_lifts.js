4159e5f46652555344ec3f3ceeeb59b76ce4abf7
Dave Harms <daveharmswebdev@gmail.com>
2016-12-12 11:12:14 -0600

    added migration for lifts

--- migrations/20161212105848_lifts.js
+++ migrations/20161212105848_lifts.js
@@ -4,15 +4,23 @@ exports.up = (knex, Promise) => {
 
 	return Promise.all([
 
+		knex.schema.createTable('lifts', table => {
+			table.integer('lift_id').unique()
+			table.string('lift_name')
+		}),
+
 		knex.schema.createTable('personal_records', table => {
 			table.increments('pr_id')
 			table.integer('profile_id')
 				.references('profile_id')
 				.inTable('profile')
 			table.integer('lift_id')
+				.references('lift_id')
+				.inTable('lifts')
 			table.float('weight')
       table.timestamp('pr_date').defaultTo(knex.fn.now())
 		})
+
 	])
   
 }
@@ -20,7 +28,8 @@ exports.up = (knex, Promise) => {
 exports.down = (knex, Promise) => {
 
 	return Promise.all([
-		knex.schema.dropTableIfExists('personal_records')
+		knex.schema.dropTableIfExists('personal_records'),
+		knex.schema.dropTableIfExists('lifts')
 	])
 
 }
